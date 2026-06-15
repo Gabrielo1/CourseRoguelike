e19b43f33b45baf590581e6ee30a3d300372da93
Gabriel Andrade Castillo <gabriel_ac@hotmail.es>
2026-06-15 16:57:14 -0400

    Static mesh added in BP_EplosiveBarrel

--- Source/CourseRoguelike/Private/CRExplosiveBarrel.cpp
+++ Source/CourseRoguelike/Private/CRExplosiveBarrel.cpp
@@ -9,6 +9,7 @@ ACRExplosiveBarrel::ACRExplosiveBarrel()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//MeshComp->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 }
 
 // Called when the game starts or when spawned
--- Source/CourseRoguelike/Public/CRExplosiveBarrel.h
+++ Source/CourseRoguelike/Public/CRExplosiveBarrel.h
@@ -18,6 +18,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> MeshComp;
 
 public:	
 	// Called every frame
