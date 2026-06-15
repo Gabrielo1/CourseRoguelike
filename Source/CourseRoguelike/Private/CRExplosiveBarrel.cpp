0f1a54d9b2789d9bc2f713018c2e7f277ba84a05
Gabriel Andrade Castillo <gabriel_ac@hotmail.es>
2026-06-15 16:28:25 -0400

    Explosive barrel class and BP was created.
    In C++, Explosive Barrel inherits from the Actor class.

---
+++ Source/CourseRoguelike/Private/CRExplosiveBarrel.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CRExplosiveBarrel.h"
+
+// Sets default values
+ACRExplosiveBarrel::ACRExplosiveBarrel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACRExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACRExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
