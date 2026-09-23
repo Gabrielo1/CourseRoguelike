3f0121965e008163c6399d4f1b9050e4f145e766
Gabriel Andrade Castillo <gabriel_ac@hotmail.es>
2026-09-22 22:06:18 -0300

    Item Chest was added

---
+++ Source/CourseRoguelike/Private/CRItemChest.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CRItemChest.h"
+
+// Sets default values
+ACRItemChest::ACRItemChest()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACRItemChest::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACRItemChest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
--- Source/CourseRoguelike/Public/CRGameplayInterface.h
+++ Source/CourseRoguelike/Public/CRGameplayInterface.h
@@ -22,4 +22,7 @@ class COURSEROGUELIKE_API ICRGameplayInterface
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
+
+	UFUNCTION(BlueprintNativeEvent)
+	void Interact(APawn* InstigatorPawn);
 };
