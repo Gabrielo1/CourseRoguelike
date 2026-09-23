99c2c86b171d1698333ca6098ae12aa771259d81
Gabriel Andrade Castillo <gabriel_ac@hotmail.es>
2026-09-22 21:48:03 -0300

    Empty gameplay interface files was added

---
+++ Source/CourseRoguelike/Private/CRGameplayInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CRGameplayInterface.h"
+
+// Add default functionality here for any ICRGameplayInterface functions that are not pure virtual.
